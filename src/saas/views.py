d75bd50b42e22cc6fd62d60d24bb325b200d6338
zilit <zili.tnd@gmail.com>
2025-01-03 01:37:58 +0200

    initial commit, basic Django application

---
+++ src/saas/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+
+from visits.models import PageVisits
+
+
+def home_view(request, *args, **kwargs):
+    return about_view(request, *args, **kwargs)
+
+def about_view(request, *args, **kwargs):
+    qs = PageVisits.objects.all()
+    page_qs = PageVisits.objects.filter(path=request.path)
+    title = "About"
+    try:
+        percent = (page_qs.count() * 100) / qs.count()
+    except Exception:
+        percent = 0
+
+    context = {
+        "page_title": title,
+        "page_visit_count": page_qs.count(),
+        "percent": percent,
+        "total_visit_count": qs.count()
+    }
+    PageVisits.objects.create(path=request.path)
+    html_template = "home.html"
+    return render(request, html_template, context)
